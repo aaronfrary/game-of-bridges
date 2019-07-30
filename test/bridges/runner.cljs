0ea49333f5b387f66ae926573c684fd05555b0f8
Aaron Frary <af@aaronfrary.com>
2019-07-30 16:57:43 -0400

    Add some logic tests.

--- test/bridges/runner.cljs
+++ test/bridges/runner.cljs
@@ -1,5 +1,10 @@
 (ns bridges.runner
     (:require [doo.runner :refer-macros [doo-tests]]
-              [bridges.util-test]))
+              [bridges.util-test]
+              [bridges.logic-test]
+              ))
 
-(doo-tests 'bridges.util-test)
+(doo-tests
+    'bridges.util-test
+    'bridges.logic-test
+    )
