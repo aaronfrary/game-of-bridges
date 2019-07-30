fe58a34f4efd3e0ecd011199a2943672e3b48361
Aaron Frary <af@aaronfrary.com>
2019-07-30 16:24:27 -0400

    Copy over remaining util functions.

--- src/bridges/util.cljs
+++ src/bridges/util.cljs
@@ -1,11 +1,50 @@
 (ns bridges.util
   (:require [clojure.set]))
 
+(defn strcmp [s1 s2]
+  (compare (str s1) (str s2)))
+
+(def str< (comp neg? strcmp))
+
+(defn keys= [ks & args]
+  (every? #(apply = (map % args)) ks))
+
+(defn set= [& args]
+  (apply = (map set args)))
+
+(defn intersect [& args]
+  (apply clojure.set/intersection (map set args)))
+
+(defn union [& args]
+  (apply clojure.set/union (map set args)))
+
+(defn set-diff [& args]
+  (apply clojure.set/difference (map set args)))
+
 (defn pairwise [f [hd & tl]]
   (if (seq tl)
     (concat (map (partial f hd) tl)
             (pairwise f tl))
     nil))
+
+(defn make-item-getter
+  "Function factory for getting nearest item to a square in a direction."
+  [x-compare y-compare xy-key first-or-last]
+  (fn getter ([{:keys [x y]} items] (getter x y items))
+    ([x y items]
+     (->> items
+          (filter #(and (x-compare (:x %) x) (y-compare (:y %) y)))
+          (sort-by xy-key)
+          (first-or-last)))))
+
+(defn get-item [direction & args]
+  (apply (case direction
+           :up    (make-item-getter = > :y first)
+           :down  (make-item-getter = < :y last)
+           :left  (make-item-getter < = :x last)
+           :right (make-item-getter > = :x first)
+           (fn [& _] nil))
+         args))
 
 ;;; Union Find algorithm for graph connectedness
 
