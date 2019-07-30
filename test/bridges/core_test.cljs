ea100917d901be54ec3e7c98549ab47f5c258613
Aaron Frary <af@aaronfrary.com>
2019-07-30 16:16:46 -0400

    Remove unused test file

--- test/bridges/core_test.cljs
+++
@@ -1,7 +0,0 @@
-(ns bridges.core-test
-  (:require [cljs.test :refer-macros [deftest testing is]]
-            [bridges.core :as core]))
-
-(deftest fake-test
-  (testing "fake description"
-    (is (= 1 2))))
